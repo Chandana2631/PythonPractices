ccc79c0afcb0e2466983183ba319a1f82b8268a0
Chandana2631 <chandanaverriboyina@gmail.com>
2024-10-16 18:40:30 +0530

    Added second program

---
+++ Basics/_2_variable_reading.py
@@ -0,0 +1,13 @@
+# variable_reading.py
+
+# Declare variables
+name = "Bob" # String variable
+age = 25 # Integer variable
+height = 5.5 # Float variable
+is_student = True # Boolean variable
+
+# Print variables
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Is Student:",is_student)
